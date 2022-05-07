ae0736694bc4b7eb3511cfacde29144335c215a5
Nicholas Hemenway <nhemenway2013@gmail.com>
2022-05-07 14:04:42 -0500

    init simulation working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
 
 class BouncingBall():
     
@@ -12,6 +13,8 @@ class BouncingBall():
         self.k = k
         self.c = c
         
+        BouncingBall.contact_event.terminal = True
+        
     def in_air(self,t,y):
         
         x1, x2 = y
@@ -23,6 +26,22 @@ class BouncingBall():
         x1_dot = x2
         x2_dot = -1/self.m*(self.c*x2 + self.k*x1)
         return [x1_dot, x2_dot]
+    
+    def contact_event(self, t, y):
+        x1, x2 = y
+        return x1 - self.r
+    
+    def simulate(self, t_span, x0):
+        
+        sol = solve_ivp(self.in_air, t_span, x0, events=[self.contact_event], max_step=0.1)
+        return sol
+    
+b = BouncingBall(1, 1, 1)
+
+sol = b.simulate([0,10], [2, 0])
+
+fig, ax = plt.subplots()
+ax.plot(sol.t, sol.y[0,:])
     
     
     
